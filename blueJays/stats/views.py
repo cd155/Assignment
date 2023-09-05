c08238a2d3b444198ab6d6de463cf0f27c85fa0a
cd155 <cd155@163.com>
2023-09-05 13:00:00 -0400

    2 to 3

--- blueJays/stats/views.py
+++ blueJays/stats/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html', {'board_n': range(0, 2),
+    return render(request, 'index.html', {'board_n': range(0, 3),
                                           'news_n': range(0, 4)})
