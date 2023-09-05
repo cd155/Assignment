35e77919d16c24c4bb64658e4c3e2946da575b3e
cd155 <cd155@163.com>
2023-09-05 19:06:01 -0400

    view and url

--- blueJays/stats/urls.py
+++ blueJays/stats/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('team/<int:pk>', views.team, name="team"),
+    path('player/<int:pk>', views.player, name="player"),
 ]
--- blueJays/stats/views.py
+++ blueJays/stats/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html', {'board_n': range(0, 3),
                                           'news_n': range(0, 4)})
+
+
+def team(request, pk):
+    return render(request, 'team.html', {})
+
+
+def player(request, pk):
+    return render(request, 'player.html', {})
