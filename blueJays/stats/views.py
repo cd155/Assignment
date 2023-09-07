8978dc069c3ed91cc97394d1647ff735a2999845
cd155 <cd155@163.com>
2023-09-06 23:31:43 -0400

    retrieve data from apis

--- blueJays/stats/views.py
+++ blueJays/stats/views.py
@@ -2,7 +2,26 @@ import requests
 from xml.etree import ElementTree
 from django.shortcuts import render
 
+url = 'https://statsapi.mlb.com'
+url_log = 'https://www.mlbstatic.com/team-logos'
+
 def home(request):
+    divisions()
+    return render(request, 'index.html', 
+        {'divisions_data': divisions(), 
+         'news_lst': news()})
+
+
+def team(request, pk):
+    return render(request, 'team.html', {})
+
+
+def player(request, pk):
+    return render(request, 'player.html', {})
+
+
+# get news feeds data
+def news():
     news_feeds = requests.get('https://www.mlb.com/feeds/news/rss.xml')
     tree = ElementTree.fromstring(news_feeds.content)
 
@@ -16,21 +35,70 @@ def home(request):
             'title': item.find('title').text,
             'link': item.find('link').text,
             'date': item.find('pubDate').text[0:16],
-            'creator': item.find("{http://purl.org/dc/elements/1.1/}creator").text,
+            'creator': item.find('{http://purl.org/dc/elements/1.1/}creator').text or '',
             'image_url': item.find('image').attrib['href'],
         }
         news_lst.append(news_dict)
         i+=1
-
-    return render(request, 'index.html', {'board_n': range(0,3),
-                                          'news_lst': news_lst})
+    return news_lst
 
 
- 
+def divisions():
+    data = requests.get(url+'/api/v1/standings?leagueId=103,104')
+    standings = data.json()["records"]
+    standing_lst = []
+    for standing in standings:
+        standing_dict = {
+            'division_abbr': division_name_helper(standing['division']['link']),
+        }
 
-def team(request, pk):
-    return render(request, 'team.html', {})
+        team_list = []
+        for team in standing['teamRecords']:
+            team_dict = {
+                'team_abbr': team['team']['name'],
+                'team_log_url': f"{url_log}/{team['team']['id']}.svg",
+                'leagueRecord': team['leagueRecord'],
+                'gamesBack': team['gamesBack'],
+                'records': team_record_helper(team['records']['splitRecords']),
+            }
+            team_list.append(team_dict)
 
+        standing_dict['teams'] = team_list
 
-def player(request, pk):
-    return render(request, 'player.html', {})
+        standing_lst.append(standing_dict)
+
+    return data_process(standing_lst)
+
+# change list with 6 items to list with 3 items
+def data_process(lst):
+    good = []
+    for i in range(0, len(lst), 2):
+        tup = (lst[i], lst[i+1])
+        if i+1 >= len(lst):
+            tup = (lst[i], {})
+        good.append(tup)
+    return good
+
+# return division short name
+def division_name_helper(link):
+    data = requests.get(url+link)
+    division = (data.json()['divisions'])[0]
+    return division['nameShort']
+
+# return team short name
+def team_name_helper(link):
+    data = requests.get(url+link)
+    team = (data.json()['teams'])[0]
+    return team['abbreviation']
+
+# return last ten records
+def team_record_helper(lst):
+    my_dict = {}
+    for item in lst:
+        if item['type'] == 'lastTen':
+            my_dict['lastTen'] = item
+        if item['type'] == 'home':
+            my_dict['home'] = item
+        if item['type'] == 'away':
+            my_dict['away'] = item
+    return my_dict
